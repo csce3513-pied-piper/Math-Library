f614391cba3d55b491102e7e0102aa18d7082a9f
ScriptDevie <57567295+ScriptDevie@users.noreply.github.com>
2021-10-26 21:41:30 -0500

    Adding metrics

---
+++ MathLibrary/Metrics.py
@@ -0,0 +1,97 @@
+def Manhattan(x, y):
+    if len(x) is not len(y):
+        raise ValueError
+
+    distance = 0
+    for i in range(len(x)):
+        distance += abs(x[i]-y[i])
+
+    return distance
+
+def Chebyshev(x, y):
+    if len(x) is not len(y):
+        raise ValueError
+
+    distance = 0
+    max = 0
+    for i in range(len(x)):
+        distance = abs(x[i]-y[i])
+        if distance < max:
+            distance = max
+
+    return distance
+
+def Euclidian(x, y):
+    if len(x) is not len(y):
+        raise ValueError
+
+    distance = 0
+    for i in range(len(x)):
+        distance += pow(y[i]-x[i], 2) ** 1/2
+
+    return distance
+
+def Canberra(x, y):
+    if len(x) is not len(y):
+        raise ValueError
+
+    distance = 0
+    for i in range(len(x)):
+        distance += abs(x[i] - y[i]) / (abs(x[i]) + abs(y[i]))
+    
+    return distance
+
+def Cosine(x, y):
+    return NotImplementedError
+
+def Hamming(x, y):
+    if len(x) is not len(y):
+        raise ValueError
+    
+    distance = 0
+    for i in range(len(x)):
+        if x[i] == y[i]:
+            distance += 1
+    
+    return distance
+
+def Discrete(x, y):
+    if len(x) is not len(y):
+        raise ValueError
+    
+    distance = 0
+    if x is not y:
+        distance = 1
+
+    return distance
+    
+def Metric(distance, x, y, z):
+    if IdentityAndIndiscernable(distance, x, y) \
+        and Symmetric(distance, x, y) \
+        and TriangleInequality(distance, x, y, z):
+            return "Possibly"
+
+    return False
+
+
+def IdentityAndIndiscernable(distance, x, y):
+    if distance(x, y) is not 0:
+        raise ValueError
+    if x is not y:
+        raise ValueError
+    if (distance(x, y) is 0 and x is not y) or (distance(x, y) is not 0 and x is y):
+        return False
+
+    return True
+
+def Symmetric(distance, x, y):
+    if distance(x, y) is distance(y, x):
+        return True
+
+    return False
+
+def TriangleInequality(distance, x, y, z):
+    if distance(x, y) <= distance(x, z) + distance(z, y):
+        return True
+
+    return False
