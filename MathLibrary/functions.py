77a0014e948d01ea51c2061bbba92ecc40bd841c
vbhalter <vbhalter@uark.edu>
2021-11-11 16:18:04 -0600

    Added test for chi sqaure GOF, mcnemar, and kruskall-wallis. Added kruskall-wallis implementation and fixed chi square GOF

--- MathLibrary/functions.py
+++ MathLibrary/functions.py
@@ -32,16 +32,110 @@ def one_group_t_test(data_set, h_mean):
 
     return t_value
 
-#Perform chi square goodness of fit test
+#Performs chi square goodness of fit test
 def chi_square_gof(observed, expected):
 
     result = 0.0
 
-    for i in range(0, len(observed) - 1):
-         temp = (observed - expected) ** 2
-         temp = temp/expected
+    #Check that observed and expected have the same number of entries
+    if not len(observed) == len(expected):
+        print("The observed and expected lists must be of the same length!")
+        return
+
+    for i in range(0, len(observed)):
+         temp = (observed[i] - expected[i]) ** 2
+         temp = temp/expected[i]
          result = result + temp
 
     #Result is the test statistic of the Chi-Square test
     return result
         
+
+#Performs McNemar's Test and returns the test statistic
+def mcnemars(group_one, group_two):
+    try:
+        #Determine possible values of dichotomous variable
+        values = {}
+        length = len(group_one)
+        for i in range(0, length):
+            if (not values.contains(group_one(i))):
+                cont_table.add(group_one(i), 0)
+        if(len(values) != 2):
+            print("McNemar's test must be performed on a dichotomous variable")
+            return
+
+        #Populate contingency table
+        cont_table = {
+            "A": 0,
+            "B": 0,
+            "C": 0,
+            "D": 0
+        }
+        for i in range(0, length):
+            if (group_one(i) == values[0]) and (group_two(i) == values[0]):
+                cont_table["A"] = cont_table["A"] + 1
+            elif (group_one(i) == values[1]) and (group_two(i) == values[0]):
+                 cont_table["B"] = cont_table["B"] + 1
+            elif (group_one(i) == values[0]) and (group_two(i) == values[1]):
+                 cont_table["C"] = cont_table["C"] + 1
+            elif (group_one(i) == values[1]) and (group_two(i) == values[1]):
+                 cont_table["D"] = cont_table["D"] + 1
+
+        #Calculate test statistic
+        t_stat = ((cont_table["B"] - cont_table["C"]) ** 2) / (cont_table["B"] + cont_table["C"])
+        t_stat = t_stat ** .5
+        return t_stat
+            
+    except IndexError:
+        print("Data sets must be of the same size!")
+
+
+#Performs Kruskal-Wallis one way analysis of variance
+def kruskal_wallis(groups):
+    #Initialize variables
+    num_groups = len(groups)
+    all_data = []
+    ranked_data = {}
+    group_lengths = []
+    group_rank_sums = []
+
+    #Check that test is valid
+    if num_groups < 2:
+        print("This test requires at least two groups!")
+        return
+
+    #Combine all lists into one and sort data
+    for group in groups:
+        group_lengths.append(len(group))
+        for entry in group:
+            all_data.append(entry)
+    all_data.sort()
+    total_entries = len(all_data)
+
+    #Assign rank to each entry. Ranks are stored in lists to handle ties.
+    i = 1
+    for entry in all_data:
+        if entry in ranked_data:
+            ranked_data[entry].append(i)
+        else:
+            ranked_data[entry] = [i]
+        i = i + 1
+
+    #Take the sum of the ranks for each group
+    j = 0
+    for group in groups:
+        group_rank_sums.append(0)
+        for entry in group:
+            group_rank_sums[j] = group_rank_sums[j] + (sum(ranked_data[entry]) / len(ranked_data[entry]))
+        j = j + 1
+
+    #Calculate H Statistic
+    q1 = 0
+    for index in range(0, num_groups):
+        q1 = q1 + ((group_rank_sums[index] ** 2) / group_lengths[index])
+
+    q2 = 12 / (total_entries * (total_entries + 1))
+
+    h_stat = (q1 * q2) - (3 * (total_entries + 1))
+
+    return h_stat
--- Tests/test_functions.py
+++ Tests/test_functions.py
@@ -16,6 +16,22 @@ def ogtt_test():
 
     print("T-value: " + str(functions.one_group_t_test(data_set, h_mean)))
 
+#Kruskal Wallis test
+def kw_test():
+    L1 = [8.2, 10.3, 9.1, 12.6, 11.4, 13.2]
+    L2 = [10.2, 9.1, 13.9, 14.5, 9.1, 16.4]
+    L3 = [13.5, 8.4, 9.6, 13.8, 17.4, 15.3]
+    groups = [L1, L2, L3]
+    print(functions.kruskal_wallis(groups))
 
-ogtt_test()
+#Chi Square GOF Test
+def csgof_test():
+    observed = [180, 250, 120, 225, 225]
+    expected = [200, 200, 200, 200, 200]
+    print(functions.chi_square_gof(observed, expected))
+
+
+#ogtt_test()
+#kw_test()
+#csgof_test()
     
